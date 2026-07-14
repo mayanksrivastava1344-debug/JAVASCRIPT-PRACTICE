b1a5820fa86acd906c622ffb9f556318b1c38753
Mayank Raj <mayankraj@Mayanks-MacBook-Air-2.local>
2026-07-14 17:52:08 +0530

    add convertion file

---
+++ 03_conversionoperation.js
@@ -0,0 +1,30 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueINNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+///"33abc" => NaN
+// true => 1; false => 0
+
+let inloggedIn = "mayank"
+
+let booleanIsLoggedIn = boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => False
+// "" => False
+// "mayank" => true
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
+
