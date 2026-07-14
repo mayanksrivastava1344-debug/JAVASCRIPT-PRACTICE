398bda879cc347b273ef158153baa634329fc8fa
Mayank Raj <mayankraj@Mayanks-MacBook-Air-2.local>
2026-07-14 17:03:43 +0530

    added datatypes file

---
+++ 02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log("mayank")
+
+let name = "mayank"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof null);  // object
+console.log(typeof undefined);   //undefined
