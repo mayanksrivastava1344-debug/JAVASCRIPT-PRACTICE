cf0818c0353741524933905dd14b4aec2098a911
Mayank Raj <mayankraj@Mayanks-MacBook-Air-2.local>
2026-07-16 22:28:03 +0530

    adde d comparison and data types summary

---
+++ datatypes-summary.js
@@ -0,0 +1,50 @@
+// primitive
+
+// 7 types : string, number , Boolean , null , undefined , symbol , BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+// const bigNumber = 344565564556475655656n
+
+
+
+// Reference (Non Primitive )
+
+// Array , Objects , Functions
+
+const heros = ["shakiman", "thom", "doga"]
+ let myObj = {
+    name: "mayank",
+    Age: 22,
+}
+
+
+const myFunction = function() {
+    console.log("Hello world");
+}
+
+
+console.log(typeof myFunction);
+
+
+//primitive typeof value while run
+
+//Number = number
+//Sting = string
+// boolean = boolean
+//null = object
+//undefined = undefined
+// symbol = symbol
+//bigInt = bigint
+
+    
